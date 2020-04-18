531195f42d8edda78932daba28b61b689a0f52a5
Lambert Ortiz <Pablo.Lambert@grupogtd.com>
2020-04-17 20:43:47 -0400

    Note access

--- src/config.js
+++ src/config.js
@@ -5,12 +5,13 @@ export default {
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://df2y6xz6c2.execute-api.us-east-1.amazonaws.com/dev/"
+    URL: "https://40yvgwfpp3.execute-api.us-east-1.amazonaws.com/dev/"
   },
   cognito: {
     REGION: "us-east-1",
     USER_POOL_ID: "us-east-1_Il9co2H9A",
     APP_CLIENT_ID: "1didccfftst9n6m0bmo41k31s",
     IDENTITY_POOL_ID: "us-east-1:d926bf03-0b84-4445-baf2-fd1c0de7587e"
-  }
+  },
+  MAX_ATTACHMENT_SIZE: 10000000
 }
